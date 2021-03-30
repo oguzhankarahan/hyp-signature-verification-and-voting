eb03f36979a9110306716ea329e70c92956d83ca
Oğuzhan Karahan <oguzhankarahan.tr@gmail.com>
2021-03-30 22:40:53 +0300

    smart account codes added

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,23 +1,10 @@
-// async function main() {
+var Voting = artifacts.require("Voting.sol");
+var SmartAccount = artifacts.require("SmartAccount.sol");
+var SmartAccountRegistry = artifacts.require("SmartAccountRegistry.sol");
 
-//     const [deployer] = await ethers.getSigners();
-
-//     console.log(
-//         "Deploying contracts with the account:",
-//         deployer.address
-//     );
-
-//     console.log("Account balance:", (await deployer.getBalance()).toString());
-
-//     const Token = await ethers.getContractFactory("Token");
-//     const token = await Token.deploy();
-
-//     console.log("Token address:", token.address);
-// }
-
-// main()
-//     .then(() => process.exit(0))
-//     .catch(error => {
-//         console.error(error);
-//         process.exit(1);
-//     });
+module.exports = function(deployer) {
+  deployer.deploy(Voting);
+  deployer.deploy(SmartAccount);
+  deployer.deploy(SmartAccountRegistry);
+  deployer.link(Voting, SmartAccount, SmartAccountRegistry);
+};
